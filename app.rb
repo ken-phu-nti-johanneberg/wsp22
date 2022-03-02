05a7d3a8aac74116885f833fdb1d912e1675ce54
ken-phu-nti-johanneberg <ken.phu@elev.ntig.se>
2022-03-02 11:41:16 +0100

    Routes, databaser

--- app.rb
+++ app.rb
@@ -4,30 +4,86 @@ require 'sqlite3'
 require 'bcrypt'
 
 enable :sessions
+# Skapa sessions med id, användaren inloggad
+# Stora eller små bokstäver i databasen???
 
+# Start
 get('/') do
     slim(:start)
 end
 
+# Profil
 get('/profile') do 
 # Ska visa olika beroende på om användaren är inloggad eller inte
     slim(:profile)
 end
 
+# Registering sida
 get('/register') do
     slim(:register)
 end
 
+# Skapa konto, skapas id automatisk?
+post('/user') do 
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new("db/ryuutama.db")
+        db.execute("INSERT INTO users (Username, Password) VALUES (?,?)", username, password_digest)
+        redirect('/showlogin')
+    else
+        "Password does not match"
+    end
+end
+
+# Login sida, (kan göra så att man två routes med '/login' men en med get och en med post)
 get('/showlogin') do
     slim(:login)
 end
 
-post('/login') do
 # Logga in
-    redirect(:profile)
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    # Hämta lösenord och id för username
+    db = SQLite3::Database.new("db/ryuutama.db")
+    # Vad gör .first???
+    db.execute("SELECT * FROM users where Username =?". username).first
+    # Vad ska stå innanför result
+    password_digest = result["password"]
+    id = result["UserID"]
+    if BCRYPT::Password.new(password_digest) == password
+        session[:id] = id 
+        redirect('/profile')
+    else
+        'Wrong password'
+    end
 end
 
-post('/user') do 
-# spara lösenord
-    redirect(:login)
+# Character_list
+get('/characters/index') do
+    slim(:"characters/index")
 end
+
+# New character
+get('/characters/new') do
+    slim(:"characters/new")
+end
+
+post('/characters/new') do
+# Ta in data från formulär från slim
+# Lägg till i data bas
+# INSERT användaren id också
+    name = params[:char_name]
+    db = SQLite3::Database.new("db/ryuutama.db")
+    db.execute("INSERT INTO character_list (Name) VALUES (?)", name)
+    redirect('/characters')
+end
+# Edit character
+# Ta in data från formulär från slim
+# Lägg till i data bas
+
+# Visa karaktär
+# Typ visa edit sidan utan att kunna ändra på saker
